781cd16ae8f69ae83713498cebea4cc9f12b8b3d
Dima Moskalenko <dmtrmsklnk@gmail.com>
2022-07-23 22:14:05 +0300

    add tab js

--- src/js/js.js
+++ src/js/js.js
@@ -1,5 +1,6 @@
 'Use strict'
 window.addEventListener('DOMContentLoaded', ()=> {
+  /* slider */
 const slides = document.querySelectorAll('.slider__img');
       prev = document.querySelector('.slider__prev');
       next = document.querySelector('.slider__next');
@@ -35,4 +36,39 @@ const slides = document.querySelectorAll('.slider__img');
         slidePlus(-1)
       })
 
+/* табы */
+const wrapper = document.querySelector('.tab__tabwrapper');
+      tabs = document.querySelectorAll('.tab__item');
+      content = document.querySelectorAll('.tab__wrappercard');
+      divintabs = document.querySelectorAll('.click');
+      function hideContent() {
+        content.forEach(elem=>{
+          elem.classList.remove('tab__showtab')
+        }) 
+
+        tabs.forEach(elem=>{
+          elem.classList.remove('tab__active')
+        })
+      }
+
+      function showTab(i) {
+        content[i].classList.add('tab__showtab')
+        tabs[i].classList.add('tab__active')
+      }
+      
+
+      wrapper.addEventListener('click', (e)=>{
+       function universe(selector, variable) {
+        if(e.target && e.target.classList.contains(selector)){
+          variable.forEach((item, i)=>{
+            if(e.target === item){
+              hideContent() 
+              showTab(i)
+            }
+          })
+         }
+       }
+       universe('tab__item', tabs);
+       universe('click', divintabs)
+      })
 })
